850dfc6d18c4678897a1dc68bcdd34d93e6a605c
southday <lichaoxi7@qq.com>
2019-01-10 23:20:34 +0800

    添加：使用动态规划方法求解“最大子数组”问题

--- src/main/java/southday/java/acm/other/README.md
+++ src/main/java/southday/java/acm/other/README.md
@@ -6,5 +6,6 @@
 - 3.最大连续子数组和
     + [MaxSubArraySumSolution.java](./solution/MaxSubArraySumSolution.java)
     + [MaxSubArraySumDAC.java](./solution/MaxSubArraySumDAC.java)
+    + [MaxSubArraySumDP.java](./solution/MaxSubArraySumDP.java)
 - 4.最优二叉搜索树的构造
     + [OptimalBSTree.java](./solution/OptimalBSTree.java)
---
+++ src/main/java/southday/java/acm/other/solution/MaxSubArraySumDP.java
@@ -0,0 +1,34 @@
+package southday.java.acm.other.solution;
+
+/* 使用动态规范化方法求解
+1）设子数组序列Si = {x1, x2, ..., xi}
+2）令w[i]表示以xi结尾的最大连续子数组的和，则有：
+w[i] = 0, i=0
+     = max(w[i-1]+xi, xi), i>0
+3）用maxv记录在遍历求解w[i]过程中连续子数组和的最大值
+ */
+
+/** 最大子数组和 - 动态规划算法
+ * Author southday
+ * Date   2019/1/10
+ */
+public class MaxSubArraySumDP {
+
+    public int maxSubArray(int A[]) {
+        int[] w = new int[A.length];
+        int maxv = 0;
+        for (int i = 1; i < w.length; i++) {
+            w[i] = w[i - 1] + A[i] >= A[i] ? w[i - 1] + A[i] : A[i];
+            if (maxv < w[i])
+                maxv = w[i];
+        }
+        return maxv;
+    }
+
+    public static void main(String[] args) {
+        MaxSubArraySumDP o = new MaxSubArraySumDP();
+        // A[0] 不使用，真正的数据从A[1]开始
+        int A[] = {0, 13, -3, -25, 20, 3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+        System.out.println("sum = " + o.maxSubArray(A));
+    }
+}
