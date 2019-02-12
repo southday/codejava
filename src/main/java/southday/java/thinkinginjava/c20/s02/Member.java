dad86d68b901cfffc31aafaa590d3596456d6c4a
southday <lichaoxi7@qq.com>
2019-02-12 17:02:58 +0800

    更新Member.java

--- src/main/java/southday/java/thinkinginjava/c20/s02/Member.java
+++ src/main/java/southday/java/thinkinginjava/c20/s02/Member.java
@@ -12,6 +12,10 @@ import southday.java.thinkinginjava.c20.s02.dbanno.SQLString;
  */
 @DBTable(name = "MEMBER")
 public class Member {
+    /* 书写快捷方式
+    如果注解中定义了名为value的元素，并且在应用该注解的时候，
+    如果该元素是唯一需要赋值的一个元素，那么此时无需使用<名,值>对的语法，而只需在括号内给出value元素所需的值即可，如下：
+     */
     @SQLString(30) String firstName;
     @SQLString(50) String lastName;
     @SQLInteger Integer age;
