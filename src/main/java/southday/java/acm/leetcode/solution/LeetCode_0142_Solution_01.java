a930df80d3182295eddb9c81673827a9c317e5c1
southday <lichaoxi7@qq.com>
2019-05-03 11:16:41 +0800

    更新 LeetCode_0142_Solution_01.java 注释，完善讲解

--- src/main/java/southday/java/acm/leetcode/solution/LeetCode_0142_Solution_01.java
+++ src/main/java/southday/java/acm/leetcode/solution/LeetCode_0142_Solution_01.java
@@ -21,9 +21,17 @@ Can you solve it without using extra space?
     c) dis(fast) - dis(slow) = n * L (即快指针在环中多走了n圈)
     d) A + B = n * L (即：(A + B) % L = 0)
 说明：
-    a) 慢指针 slow 继续走 A 的距离，刚好能走完一个完整的环，即到达 x 结点
-    b) 表头 head 走 A 的距离，刚好能到达 x 结点
+    a) 慢指针 slow 继续走 L-B=A 的距离，刚好能走完一个完整的环，即到达 x 结点
+    b) 表头 head 走 A 的距离，刚好能到达 x 结点（这在设(b)中已成立）
     c) 即：slow 和 head 同时单步走，二者相等时，即为循环开始的结点 x
+
+                             - - - - - - - - - -
+                             |                  |
+                             |                  |
+                             |                  |
+    [-] - - - - - - - - - - [-] - - - - - - - [-]
+     | <-        A        -> | <-      B    -> |
+     head                    x                 w
  */
 
 import southday.java.acm.leetcode.common.LeetCodeUtil;
